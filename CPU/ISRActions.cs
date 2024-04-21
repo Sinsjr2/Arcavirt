4959da7657c735d0c6d4664b2a9819be2e593bc8
sinsjr <hks15sirubia@gmail.com>
2024-04-21 22:52:32 +0900

    CPUへの割り込みを通知するためのクラスを追加しました。

---
+++ CPU/ISRActions.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CPU {
+
+    /// <summary>
+    /// 登録されたコールバックを割り込み要求があった時に実行します。
+    /// </summary>
+    public class ISRActions : IISRNotify {
+
+        readonly Dictionary<int, Action?> actions = new();
+
+        public void SetCallback(int irqNo, Action? action) {
+            actions[irqNo] = action;
+        }
+
+        public void SetInterrupt(int irqNo, bool value) {
+            if (!value) {
+                return;
+            }
+            if (actions.TryGetValue(irqNo, out var act)) {
+                act?.Invoke();
+            }
+        }
+    }
+}
